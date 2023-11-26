1757471ee863c52232690c2de06ffb83d2ce2325
Sorana-Maria <sorana.avramescu@gmail.com>
2023-11-26 16:36:43 +0100

    Update run2_unittest.py

--- run2_unittest.py
+++ run2_unittest.py
@@ -1,32 +1,41 @@
 import unittest
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+import time
 
 class TestTextGeneration(unittest.TestCase):
     def setUp(self):
-        # This method will be called before each test case
-        self.model_directory = "Software"
+        # Set up the model and tokenizer
+        self.model_directory = "Software"  # Adjust this if the model is saved in a different directory
         self.model = AutoModelForCausalLM.from_pretrained(self.model_directory)
         self.tokenizer = AutoTokenizer.from_pretrained(self.model_directory)
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
         self.model = self.model.to(self.device)
         self.text_generation_pipeline = pipeline(task="text-generation", model=self.model, tokenizer=self.tokenizer, device=0, max_length=2000)
 
-    def tearDown(self):
-        # This method will be called after each test case
-        pass
-
     def test_generate_text(self):
-        # Test the generate_text function with a simple prompt
         prompt = "Test prompt."
-        generated_text = generate_text(prompt)
-        self.assertTrue(isinstance(generated_text, str))
+        max_length = 1000  # Set your desired max_length for testing
 
-    def test_max_token_length(self):
-        # Test if the max token length is working as expected
-        prompt = "Test prompt."
-        max_length = 1000
-        generated_text = generate_text(prompt, max_length=max_length)
-        tokens = tokenizer(generated_text)["input_ids"]
-        self.assertTrue(len(tokens) <= max_length)
+        # Ensure the function doesn't raise any exceptions
+        with self.assertLogs(level="ERROR"):
+            output_text = self.text_generation_pipeline(prompt, max_length=max_length)
 
-if __name__ == '__main__':
+        # Ensure the output is not empty
+        self.assertTrue(output_text)
+
+    def test_generate_text_with_long_prompt(self):
+        prompt = "This is a very long prompt. " * 50
+        max_length = 2000  # Set your desired max_length for testing
+
+        # Ensure the function doesn't raise any exceptions
+        with self.assertLogs(level="ERROR"):
+            output_text = self.text_generation_pipeline(prompt, max_length=max_length)
+
+        # Ensure the output is not empty
+        self.assertTrue(output_text)
+
+    # Add more test cases as needed
+
+if __name__ == "__main__":
     unittest.main()
