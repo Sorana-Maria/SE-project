ab04518fa46ed14e0e36b4c080d82e89d971fb33
Sorana-Maria <sorana.avramescu@gmail.com>
2023-11-27 13:37:53 +0100

    Update run2_unittest.py

--- run2_unittest.py
+++ run2_unittest.py
@@ -3,37 +3,72 @@ import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 import time
 
-class TestTextGeneration(unittest.TestCase):
+class TestYourCode(unittest.TestCase):
+
     def setUp(self):
-        # Set up the model and tokenizer
-        self.model_directory = "Software" 
+        # Set up any necessary resources or configurations for tests
+        self.model_directory = "Software"
+        
+        # Load model and tokenizer
         self.model = AutoModelForCausalLM.from_pretrained(self.model_directory)
         self.tokenizer = AutoTokenizer.from_pretrained(self.model_directory)
+        
+        # Ensure model is on the appropriate device (CPU/GPU)
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
         self.model = self.model.to(self.device)
+        
+        # Create a text generation pipeline
         self.text_generation_pipeline = pipeline(task="text-generation", model=self.model, tokenizer=self.tokenizer, device=0, max_length=2000)
 
-    def test_generate_text(self):
-        prompt = "Test prompt."
-        max_length = 1000  # Set the desired max_length for testing
+    def test_model_loading(self):
+        # Test that the model and tokenizer are loaded successfully
+        self.assertIsInstance(self.model, AutoModelForCausalLM)
+        self.assertIsInstance(self.tokenizer, AutoTokenizer)
 
-        # Ensure the function doesn't raise any exceptions
-        with self.assertLogs(level="ERROR"):
-            output_text = self.text_generation_pipeline(prompt, max_length=max_length)
+    def test_text_generation_function(self):
+        # Test the generate_text function with a simple prompt
+        prompt = "Test prompt"
+        generated_text = generate_text(prompt)
+        self.assertTrue(isinstance(generated_text, str) and len(generated_text) > 0)
 
-        # Ensure the output is not empty
-        self.assertTrue(output_text)
+    def test_gpu_usage(self):
+        # Test that the model is on the correct device (GPU/CPU)
+        self.assertEqual(self.device, "cuda" if torch.cuda.is_available() else "cpu")
 
-    def test_generate_text_with_long_prompt(self):
-        prompt = "This is a very long prompt. " * 50
-        max_length = 2000  # Set the desired max_length for testing
+    def test_input_validation_max_token_length(self):
+        # Test input validation for max token length
+        input_token_length_numeric = "500"
+        result_numeric = generate_text("Test prompt", max_length=int(input_token_length_numeric))
+        self.assertTrue(isinstance(result_numeric, str) and len(result_numeric) > 0)
 
-        # Ensure the function doesn't raise any exceptions
-        with self.assertLogs(level="ERROR"):
-            output_text = self.text_generation_pipeline(prompt, max_length=max_length)
+        input_token_length_non_numeric = "abc"
+        result_non_numeric = generate_text("Test prompt", max_length=input_token_length_non_numeric)
+        self.assertTrue(isinstance(result_non_numeric, str) and len(result_non_numeric) > 0)
 
-        # Ensure the output is not empty
-        self.assertTrue(output_text)
+    def test_pipeline_creation(self):
+        # Test that the text generation pipeline is created successfully
+        self.assertIsNotNone(self.text_generation_pipeline)
 
-if __name__ == "__main__":
+    # Integration Tests
+
+    def test_end_to_end(self):
+        # Simulate a complete interaction by providing a prompt through the input
+        prompt = "End-to-End Test Prompt"
+        generated_text = generate_text(prompt)
+        self.assertTrue(isinstance(generated_text, str) and len(generated_text) > 0)
+
+    def test_performance(self):
+        # Test the performance of text generation with a long prompt
+        prompt = "Long prompt for performance testing" * 100
+        start_time = time.time()
+        generate_text(prompt)
+        duration = time.time() - start_time
+        self.assertLess(duration, 5.0)
+
+    def test_error_handling(self):
+        # Test the behavior when the model directory is incorrect or doesn't exist
+        with self.assertRaises((OSError, FileNotFoundError, transformers.modeling_utils.ConfigurationError, transformers.modeling_utils.ModelLoadConfigError)):
+            model = AutoModelForCausalLM.from_pretrained("Incorrect_Directory")
+
+if __name__ == '__main__':
     unittest.main()
